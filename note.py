e6dcee5a5b2c0c69659635adc4bd0ddfdd637638
maikon-sudo <151402771+maikon-sudo@users.noreply.github.com>
2023-11-18 18:21:15 -0300

    cafezim

---
+++ note.py
@@ -0,0 +1,7 @@
+nome =str(input('qual seu nome?'))
+print('ola {}'.format(nome))
+cafe = str(input('voce quer um cafezim ? '))
+if (cafe == 'sim'):
+    print('vou pegar pra vc, senta e relaxa')
+else :
+    print('ok, entao nao reclama.')
